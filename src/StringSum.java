24555e9277e59345fc766cf27aa2edc9f280c967
gizemsacal <46420726+gizemsacal@users.noreply.github.com>
2021-04-06 16:33:47 +0300

    Sum String

---
+++ src/StringSum.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class StringSum {
+    public static void main(String[]args){
+        String isim1= "Gizem";
+        String isim2= "Saçal";
+       String sum;
+
+     sum = isim1+ " "+ isim2;
+        System.out.println("toplam  =" + sum);
+    }
+}
